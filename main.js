224864d77950b3e236ab304f405a400f600bfdde
pongpoti <pong.poti@gmail.com>
2025-05-29 23:08:31 +0700

    c

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = express();
 const SUPABASE_URL = Deno.env.get("SUPABASE_URL");
 const SUPABASE_KEY = Deno.env.get("SUPABASE_KEY");
 
-const supabase = createClient( SUPABASE_URL, SUPABASE_KEY);
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 /*
 const { data, error } = await supabase.from("test")
     .insert(
@@ -17,13 +17,25 @@ const { data, error } = await supabase.from("test")
 */
 
 app.get("/", (req, res) => {
-    const { data, error } = supabase.from("test")
+  const { data, error } = supabase.from("test")
     .insert(
-        {
-            name : "ปองสิทธิ์",
-            surname : "โพธิคุณ"
-        }
-    )
+      {
+        name: "ปองสิทธิ์",
+        surname: "โพธิคุณ",
+      },
+    );
+  res.send(data);
+});
+
+app.post("/", (req, res) => {
+  const payload = req.body;
+  const { data, error } = supabase.from("test")
+    .insert(
+      {
+        name: payload,
+        surname: payload,
+      },
+    );
   res.send(data);
 });
 
