af49be32754ad3df0f22a481e7aa453f8f160324
pongpoti <pong.poti@gmail.com>
2025-05-29 13:08:59 +0700

    c

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ import { createClient } from 'jsr:@supabase/supabase-js@2'
 const app = express();
 //const SUPABASE_URL = 'https://zlfahkhcsxxmqswnpgfs.supabase.co';
 //const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsZmFoa2hjc3h4bXFzd25wZ2ZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0OTAyNDIsImV4cCI6MjA2NDA2NjI0Mn0.2sG581agOPFxKdMJM4sALND3cvtoqdoVLpgo5aYwHMY';
-//const SUPABASE_URL = Deno.env.get("SUPABASE_URL");
-//const SUPABASE_KEY = Deno.env.get("SUPABASE_KEY");
+const SUPABASE_URL = Deno.env.get("SUPABASE_URL");
+const SUPABASE_KEY = Deno.env.get("SUPABASE_KEY");
 
 const supabase = createClient( SUPABASE_URL, SUPABASE_KEY);
 
